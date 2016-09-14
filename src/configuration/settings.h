9dc9ecdec51c72eceea70eef7c817eed58cdc601
turnervink <turner@turnervink.com>
2016-09-14 12:39:52 -0700

    Add seconds toggle to config page

--- src/configuration/settings.h
+++ src/configuration/settings.h
@@ -15,6 +15,7 @@ typedef enum {
   CfgKeyBackgroundColour,
   CfgKeyTextColour,
   CfgKeyInvertColours,
+  CfgKeyShowSeconds,
   CfgKeyMiddleBarMode,
   CfgKeyUseAutomaticStepGoal,
   CfgKeyManualStepGoal,
